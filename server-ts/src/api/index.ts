b0f99a28ce0554e612af8b666946f1ca99c5aa33
อาลีฟริดฎูวาน อุมา <alifriduwanhellowold17@gmail.com>
2023-01-20 21:40:26 +0700

    สร้าง api userResult

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,11 +1,15 @@
 import Router from 'koa-router'
 import announcement from './announcement'
+import userResult from './user_result'
 
 const apiRouter = new Router()
 
 apiRouter.get('/api/greet', (ctx,next) => {
     ctx.body = {msg: 'Hello world'}
 })
+
 apiRouter.use('/api/announcement',announcement.routes())
+
+apiRouter.use('api/userResult',userResult.routes())
 
 export default apiRouter
