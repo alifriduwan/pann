fdceecf9c22d7794a3383e0ac9c73b3fb3dd30ad
อาลีฟริดฎูวาน อุมา <alifriduwanhellowold17@gmail.com>
2023-01-21 10:25:31 +0700

    สร้าง config เชื่อมต่อกับ db

--- server-ts/src/db.ts
+++ server-ts/src/db.ts
@@ -1,8 +1,10 @@
 import knex from 'knex'
+import appConfig from './config';
 const knexStringcase = require('knex-stringcase');
 
 const db = knex(knexStringcase({
   client: 'mysql2',
+  connection:appConfig.dbconnectinfo,
   useNullAsDefault: true,
 }));
 
