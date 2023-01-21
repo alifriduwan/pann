8029c57243b8bd02d3066163197f8bc0857c16e3
อาลีฟริดฎูวาน <alifriduwanhellowold@gmail.com>
2023-01-21 17:38:40 +0700

    เพิ่มการดึง public key

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -1,17 +1,27 @@
 import Koa from 'koa'
 import koaBody from 'koa-body'
-import json from 'koa-json'
 import apiRouter from './api'
 import appConfig from './config'
 import loadFixtures from './fixture'
+import cors from '@koa/cors'
+import { initSsoCert } from './auth'
 
 const app = new Koa()
 
-app.use(json())
-app.use(apiRouter.routes())
+app.use(cors());
 app.use(koaBody())
+app.use(apiRouter.routes());
 
-app.listen(8000)
+(async () => {
+  await loadFixtures(appConfig.ClearDataBeforeLoad)
+  //await initSsoCert()
+  app.listen(8000)
+  console.log('Server is ready at port 8000')
+})();
+
+
+
+
 
 
 
