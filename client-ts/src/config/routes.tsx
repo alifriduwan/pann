912a351753f1e513c7125d41e23906ba14afca62
อาลีฟริดฎูวาน <alifriduwanhellowold@gmail.com>
2023-01-23 13:46:17 +0700

    เพิ่ม User Result List Page as Home

--- client-ts/src/config/routes.tsx
+++ client-ts/src/config/routes.tsx
@@ -2,14 +2,17 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 
 import Login from "../page/login"
+import UserResultList from "../page/user-result-list"
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route index element={<Login />} />
             <Route path="login" element={<Login />}/>
+            <Route path="home" element={<UserResultList/>} />
         </Routes>
     )
 }
+
 
 export default AppRoutes
