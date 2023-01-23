e25251ecd87aa6608fd0908722468bb0984a09e1
อาลีฟริดฎูวาน <alifriduwanhellowold@gmail.com>
2023-01-23 14:04:42 +0700

    Add staff Authentication

--- client-ts/src/config/routes.tsx
+++ client-ts/src/config/routes.tsx
@@ -1,18 +1,39 @@
 import React from "react"
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"
+import { useAppCtx } from "../AppProvider"
 
 import Login from "../page/login"
 import UserResultList from "../page/user-result-list"
 
+type Props = {
+    staffOnly?: boolean
+    children: JSX.Element
+  }
+  
+  const ProtectedRoute = ({staffOnly, children }: Props) => {
+    const {userInfo, action} = useAppCtx();
+    const location = useLocation()
+    const staffDenied = staffOnly && !action.isStaff()
+    if (!userInfo.ready || staffDenied) {    
+      if(staffDenied){
+        action.signOut()
+      }
+      console.log('backTo = ', location.pathname)
+      return  <Navigate to="/login" replace state={{backTo: location.pathname}}/>;
+    }
+  
+    return children;
+  }
+  
 const AppRoutes = () => {
     return (
         <Routes>
             <Route index element={<Login />} />
             <Route path="login" element={<Login />}/>
-            <Route path="home" element={<UserResultList/>} />
+            <Route path="home" element={<ProtectedRoute><UserResultList/></ProtectedRoute>} />
         </Routes>
-    )
-}
+    );
+};
 
 
 export default AppRoutes
