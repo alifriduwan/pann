57977228d6a5d14915685244ccb49f6624e23387
อาลีฟริดฎูวาน <alifriduwanhellowold@gmail.com>
2023-01-23 12:24:07 +0700

    แก้ไข App ให้มีการ auth

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,69 +1,21 @@
-import React from 'react';
+import React from "react";
+import { AuthProvider } from "react-oidc-context";
+import AppProvider from "./AppProvider";
+import AppRoutes from "./config/routes";
 import './App.css';
-import Announcement from './models/Announcement';
-import { useEffect,useState}  from 'react'
-import Repo from './repositories'
-import UserResult from './models/UserResult';
-
-function App() {
-  const [announcementList, setAnnouncementList] = useState<Announcement[]>([])
-  const [userResultList, setuserResultList] = useState<UserResult[]>([])
-
-  const fetchannouncementList = async () => {
-    const result = await Repo.announcements.getAll()
-    if(result){
-      setAnnouncementList(result)
-    }
-  }
-  const fetchuserResultList = async () => {
-    const result = await Repo.UserResult.getAll()
-    if(result){
-      setuserResultList(result)
-    }
-  }
+import { oidcConfig } from './config';
 
 
-  useEffect(() => {
-    fetchannouncementList()
-    fetchuserResultList()
-  })
 
+function App(){
   return (
-    <div>
-      {announcementList.map(announcement => (
-        <div key={announcement.id}>
-          <p>ID : {announcement.id}</p>
-          <p>Description : {announcement.description}</p>
-          <p>Positive : {announcement.remarkIfPositive}</p>
-          <p>Negative : {announcement.remarkIfNegative}</p>
-          <p>Time : {announcement.pubDateTime.toLocaleString('en-US', {
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-              second: '2-digit'
-            })}</p>
-          <p>UserCode : {announcement.userCode}</p>
-        </div>
-      ))}
-                  <hr />
-      <div>
-        {userResultList.map(userResult => (
-          <div key={userResult.id}>
-            <p>ID : {userResult.id}</p>
-            <p>Topic : {userResult.announcement.topic}</p>
-            <p>Result : {userResult.result}</p>
-            <p>UserCode : {userResult.userCode}</p>
-          </div>
-        ))}
-      </div>
+    <AuthProvider {...oidcConfig}>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>      
+  </AuthProvider>
+)
 
-
-      
-    </div>
-
-  );
 }
 
 export default App;
